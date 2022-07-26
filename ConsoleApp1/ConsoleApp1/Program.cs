dd0afd516c84b15b13d5c4844cfa00ae2f48a768
svmarkst <s.s.mark@yandex.ru>
2022-07-26 20:45:17 +0300

    Работает

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test
+{
+    class TestClass
+    {
+        public static int summ(int a, int b)
+        {
+            int res;
+            res = a + b;
+            return res;
+        }
+        
+        public static void Main(string[] args)
+        {
+            int nuumber;
+            nuumber = summ(4, 3);
+            Console.WriteLine(nuumber);
+            Console.ReadKey();
+
+        }
+
+    }
+}
